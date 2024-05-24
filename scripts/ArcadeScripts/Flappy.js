5730b44c3e4480f16fb474ddbc2d974fe47f45a1
MindlessTruffle <82486398+MindlessTruffle@users.noreply.github.com>
2024-05-24 13:28:59 +0000

    Json test

--- scripts/ArcadeScripts/Flappy.js
+++ scripts/ArcadeScripts/Flappy.js
@@ -60,7 +60,7 @@ window.onload = function () {
     bottomPipeImg = new Image();
     bottomPipeImg.src = "../../images/ArcadeImages/Flappy/bottompipe.png";
 
-    highScore = getCookie("flappyBirdHighScore") || 0;
+    highScore = getHighScores()[0] || 0; // Get the highest score
 
     requestAnimationFrame(update);
     setInterval(placePipes, 1500); //every 1.5 seconds
@@ -173,39 +173,35 @@ function detectCollision(a, b) {
 }
 
 function checkHighScore() {
-    if (score > highScore) {
-        highScore = score;
-        setCookie("flappyBirdHighScore", highScore, 365);
+    let highScores = getHighScores();
+    highScores.push(score);
+    highScores.sort((a, b) => b - a); // Sort descending
+    if (highScores.length > 10) {
+        highScores.pop(); // Keep only top 10 scores
     }
+    setHighScores(highScores);
 }
 
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+function setHighScores(scores) {
+    document.cookie = "flappyBirdHighScores=" + JSON.stringify(scores) + "; path=/";
 }
 
-function getCookie(name) {
-    let nameEQ = name + "=";
+function getHighScores() {
+    let name = "flappyBirdHighScores=";
     let ca = document.cookie.split(';');
     for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return JSON.parse(c.substring(name.length, c.length));
     }
-    return null;
+    return [];
 }
 
 function showLeaderboard() {
-    let cookies = document.cookie.split(';').map(cookie => cookie.trim());
-    let scores = cookies
-        .filter(cookie => cookie.startsWith('flappyBirdHighScore'))
-        .map(cookie => parseInt(cookie.split('=')[1]))
-        .sort((a, b) => b - a);
-
-    alert("Leaderboard:\n" + scores.join('\n'));
+    let highScores = getHighScores();
+    let leaderboard = "Leaderboard:\n";
+    highScores.forEach((score, index) => {
+        leaderboard += (index + 1) + ". " + score + "\n";
+    });
+    alert(leaderboard);
 }
