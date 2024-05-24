8efae3a80b3d970eba313cc42a7fe4e14707970e
MindlessTruffle <82486398+MindlessTruffle@users.noreply.github.com>
2024-05-24 13:02:25 +0000

    leaderboard flappy

--- scripts/ArcadeScripts/Flappy.js
+++ scripts/ArcadeScripts/Flappy.js
@@ -1,4 +1,3 @@
-
 //board
 let board;
 let boardWidth = 360;
@@ -8,15 +7,15 @@ let context;
 //bird
 let birdWidth = 34; //width/height ratio = 408/228 = 17/12
 let birdHeight = 24;
-let birdX = boardWidth/8;
-let birdY = boardHeight/2;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
 let birdImg;
 
 let bird = {
-    x : birdX,
-    y : birdY,
-    width : birdWidth,
-    height : birdHeight
+    x: birdX,
+    y: birdY,
+    width: birdWidth,
+    height: birdHeight
 }
 
 //pipes
@@ -36,8 +35,9 @@ let gravity = 0.4;
 
 let gameOver = false;
 let score = 0;
+let highScore = 0;
 
-window.onload = function() {
+window.onload = function () {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
@@ -50,7 +50,7 @@ window.onload = function() {
     //load images
     birdImg = new Image();
     birdImg.src = "../../images/ArcadeImages/Flappy/flappybird.png";
-    birdImg.onload = function() {
+    birdImg.onload = function () {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
 
@@ -59,6 +59,8 @@ window.onload = function() {
 
     bottomPipeImg = new Image();
     bottomPipeImg.src = "../../images/ArcadeImages/Flappy/bottompipe.png";
+
+    highScore = getCookie("flappyBirdHighScore") || 0;
 
     requestAnimationFrame(update);
     setInterval(placePipes, 1500); //every 1.5 seconds
@@ -80,6 +82,7 @@ function update() {
 
     if (bird.y > board.height) {
         gameOver = true;
+        checkHighScore();
     }
 
     //pipes
@@ -95,6 +98,7 @@ function update() {
 
         if (detectCollision(bird, pipe)) {
             gameOver = true;
+            checkHighScore();
         }
     }
 
@@ -105,11 +109,12 @@ function update() {
 
     //score
     context.fillStyle = "white";
-    context.font="45px sans-serif";
+    context.font = "45px sans-serif";
     context.fillText(score, 5, 45);
 
     if (gameOver) {
         context.fillText("GAME OVER", 5, 90);
+        context.fillText("High Score: " + highScore, 5, 135);
     }
 }
 
@@ -121,26 +126,26 @@ function placePipes() {
     //(0-1) * pipeHeight/2.
     // 0 -> -128 (pipeHeight/4)
     // 1 -> -128 - 256 (pipeHeight/4 - pipeHeight/2) = -3/4 pipeHeight
-    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);
-    let openingSpace = board.height/4;
+    let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+    let openingSpace = board.height / 4;
 
     let topPipe = {
-        img : topPipeImg,
-        x : pipeX,
-        y : randomPipeY,
-        width : pipeWidth,
-        height : pipeHeight,
-        passed : false
+        img: topPipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
     }
     pipeArray.push(topPipe);
 
     let bottomPipe = {
-        img : bottomPipeImg,
-        x : pipeX,
-        y : randomPipeY + pipeHeight + openingSpace,
-        width : pipeWidth,
-        height : pipeHeight,
-        passed : false
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
     }
     pipeArray.push(bottomPipe);
 }
@@ -162,7 +167,45 @@ function moveBird(e) {
 
 function detectCollision(a, b) {
     return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
-           a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
-           a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
-           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+        a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
+        a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
+        a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+}
+
+function checkHighScore() {
+    if (score > highScore) {
+        highScore = score;
+        setCookie("flappyBirdHighScore", highScore, 365);
+    }
+}
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function showLeaderboard() {
+    let cookies = document.cookie.split(';').map(cookie => cookie.trim());
+    let scores = cookies
+        .filter(cookie => cookie.startsWith('flappyBirdHighScore'))
+        .map(cookie => parseInt(cookie.split('=')[1]))
+        .sort((a, b) => b - a);
+
+    alert("Leaderboard:\n" + scores.join('\n'));
 }
